e1ee61fc9eb5c317d37e14f44ae6258c73c454d3
RaminMT <RaminMT@outlook.com>
2020-05-30 15:25:59 +0430

    Initial upload

---
+++ FCM Message Sender/FcmModel.cs
@@ -0,0 +1,10 @@
+﻿using FCM.Net;
+
+namespace FCM_Message_Sender
+{
+    public class FcmModel
+    {
+        public string FcmKey { get; set; }
+        public Message Message { get; set; }
+    }
+}
---
+++ FCM Message Sender/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using FCM.Net;
+
+namespace FCM_Message_Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = File.ReadAllText("runtimeconfig.json");
+            var model = JsonSerializer.Deserialize<FcmModel>(jsonString);
+            
+            using var sender = new Sender(model.FcmKey);
+            var response = sender.SendAsync(model.Message).GetAwaiter().GetResult();
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(JsonSerializer.Serialize(response));
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
